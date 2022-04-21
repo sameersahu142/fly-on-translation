e920e9a1c226b6ad1630ad7569a653881412287d
sameersahu142 <sameer678sahu@gmail.com>
2022-04-21 20:35:32 +0530

    translation content update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const { isShow, isRtl } = useHideShowContext();
   const [isShowContainer, setIsShowContainer] = useState(isShow);
   const [isRightToLeft, setIsRightToLeft] = useState(isRtl);
+  const [selectTextValue, setSelectTextValue]=useState("");
+
   const handelClose = (value) => {
     setIsShowContainer(value)
   }
@@ -16,12 +18,17 @@ function App() {
     setIsRightToLeft(value)
   }
 
+  const getSelectText=(value)=>{
+    setSelectTextValue(value)
+  }
+
   return (
     <div>
       <HideShowContextProvider isHideShow={isShowContainer} isRightToLeft={isRightToLeft} >
         <TranslationTool handelClose={handelClose} handelRtlController={handelRtlController}></TranslationTool>
       </HideShowContextProvider>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> */}
+      <TranslationContent getSelectText={getSelectText}/>
     </div>
   );
 }
--- src/components/translation-content.jsx
+++ src/components/translation-content.jsx
@@ -1,27 +1,46 @@
 import React, { useEffect, useRef } from "react";
 
-const TranslationContent = () => {
+const TranslationContent = ({ getSelectText }) => {
   const getContent = useRef();
+  function getIframeSelectionText(iframe) {
+    let win = iframe.current.contentWindow;
+    let doc = win.document;
+
+    if (win.getSelection) {
+      return win.getSelection().toString();
+    } else if (doc.selection && doc.selection.createRange) {
+      return doc.selection.createRange().text;
+    }
+  }
 
   useEffect(() => {
     if (getContent) {
-      console.log(getContent);
-      getContent.current.contentWindow.getSelection();
+      getContent.current.contentWindow.addEventListener(
+        "mouseup",
+        function (event) {
+          if (getIframeSelectionText(getContent).length > 0) {
+            getSelectText(getIframeSelectionText(getContent));
+          }
+        }
+      );
     }
   }, [getContent]);
 
   return (
-    <iframe
-      ref={getContent}
-      title="Questionmark OnPremise"
-      id="deliveryWrapperFrame"
-      name="deliveryWrapperFrame"
-      src="http://localhost:4200/assets/test.html"
-      frameBorder="0"
-      scrolling="yes"
-      height="800px"
-      width="100%"
-    ></iframe>
+    <>
+      <iframe
+        ref={getContent}
+        title="Questionmark OnPremise"
+        id="deliveryWrapperFrame"
+        name="deliveryWrapperFrame"
+        src="./ifream-content/test.html"
+        frameBorder="0"
+        scrolling="yes"
+        height="800px"
+        width="100%"
+      ></iframe>
+    </>
   );
 };
+
 export default TranslationContent;
