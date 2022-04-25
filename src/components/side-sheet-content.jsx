fe72496a26b0966252db42e1a4bdaea6100b5354
sameersahu142 <sameer678sahu@gmail.com>
2022-04-25 21:14:09 +0530

    Translate area css fix

--- src/components/side-sheet-content.jsx
+++ src/components/side-sheet-content.jsx
@@ -1,113 +1,151 @@
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 import { useLanguageAndSelectTextContext } from "./context/languageAndSelectTextContext";
 import { translateYourSelectedText } from "../end-point";
 import { useHideShowContext } from "./context/hideShowContext";
-import './side-sheet.css';
+import "./side-sheet.css";
 import ExpandAndCrossIcon from "./expandAndCrossIcon";
 import useTranslation from "./localization/customHooks/translations";
 
-const SideSheetContent=({ handelClose, handelExpand })=>{
-    const { isExpand } = useHideShowContext();
-    const {selectText}=useLanguageAndSelectTextContext();
-    const [selectLanguage, setSelectLanguage] = useState("");
-    // const [selectText, setSelectText] = useState("");
-    const [translatedText, setTranslatedText] = useState("");
-    const translation = useTranslation();
-    const [isLoading, setIsLoading] = useState(false);
+const SideSheetContent = ({ handelClose, handelExpand }) => {
+  const { isExpand } = useHideShowContext();
+  const { selectText } = useLanguageAndSelectTextContext();
+  const [selectLanguage, setSelectLanguage] = useState("");
+  // const [selectText, setSelectText] = useState("");
+  const [translatedText, setTranslatedText] = useState("");
+  const translation = useTranslation();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const availableLanguages = [
-        { Code: "zh", Display: "Chinese, Simplified (简体中文)" },
-        { Code: "en", Display: "English" },
-        { Code: "ja", Display: "Japanese (日本語)" },
-        { Code: "ru", Display: "Russian (Русский)" },
-        { Code: "fr", Display: "French (Français)" },
-        { Code: "ko", Display: "Korean (한국어)" },
-        { Code: "es", Display: "Spanish (Español)" },
-        { Code: "de", Display: "German (Deutsch)" },
-        { Code: "pt", Display: "Portuguese (Português)" },
-        { Code: "it", Display: "Italian (Italiano)" },
-        { Code: "ar", Display: "Arabic (العربية)" },
-    ];
+  const availableLanguages = [
+    { Code: "zh", Display: "Chinese, Simplified (简体中文)" },
+    { Code: "en", Display: "English" },
+    { Code: "ja", Display: "Japanese (日本語)" },
+    { Code: "ru", Display: "Russian (Русский)" },
+    { Code: "fr", Display: "French (Français)" },
+    { Code: "ko", Display: "Korean (한국어)" },
+    { Code: "es", Display: "Spanish (Español)" },
+    { Code: "de", Display: "German (Deutsch)" },
+    { Code: "pt", Display: "Portuguese (Português)" },
+    { Code: "it", Display: "Italian (Italiano)" },
+    { Code: "ar", Display: "Arabic (العربية)" },
+  ];
 
-    const handelExpandBtn = (value) => {
-        handelExpand(value)
-    }
+  const handelExpandBtn = (value) => {
+    handelExpand(value);
+  };
 
-    const translate = (selectLanguage, selectText) => {
-        setIsLoading(true);
-        translateYourSelectedText(selectLanguage, selectText).then((response) => {
-            if (response.data.translated_text) {
-                setIsLoading(false);
-                setTranslatedText(response.data.translated_text);
-                // setSelectText("");
-                // setSelectLanguage("")
-            }
-        }).catch((error) => {
-            console.log(error);
-        })
-    }
-
-    function getSelectionText() {
-        let text = "";
-        let activeEl = document.activeElement;
-        let activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
-        if (
-            (activeElTagName === "textarea") || ((activeElTagName === "input") && (/^(?:text|search|password|tel|url)$/i.test(activeEl.type))) && (typeof activeEl.selectionStart === "number")
-        ) {
-            text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
-        } else if (window.getSelection) {
-            text = window.getSelection().toString();
+  const translate = (selectLanguage, selectText) => {
+    setIsLoading(true);
+    translateYourSelectedText(selectLanguage, selectText)
+      .then((response) => {
+        if (response.data.translated_text) {
+          setIsLoading(false);
+          setTranslatedText(response.data.translated_text);
+          // setSelectText("");
+          // setSelectLanguage("")
         }
-        return text;
-    }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    useEffect(() => {
-        document.onmouseup = document.onkeyup = document.onselectionchange = function () {
-            if (getSelectionText().length > 0) { 
-                // setSelectText(getSelectionText())
-             }
+  function getSelectionText() {
+    let text = "";
+    let activeEl = document.activeElement;
+    let activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
+    if (
+      activeElTagName === "textarea" ||
+      (activeElTagName === "input" &&
+        /^(?:text|search|password|tel|url)$/i.test(activeEl.type) &&
+        typeof activeEl.selectionStart === "number")
+    ) {
+      text = activeEl.value.slice(
+        activeEl.selectionStart,
+        activeEl.selectionEnd
+      );
+    } else if (window.getSelection) {
+      text = window.getSelection().toString();
+    }
+    return text;
+  }
+
+  useEffect(() => {
+    document.onmouseup =
+      document.onkeyup =
+      document.onselectionchange =
+        function () {
+          if (getSelectionText().length > 0) {
+            // setSelectText(getSelectionText())
+          }
         };
-    }, [document.onmouseup, document.onkeyup, document.onselectionchange])
+  }, [document.onmouseup, document.onkeyup, document.onselectionchange]);
 
-    useEffect(() => {
-        if (selectLanguage.length > 0 && selectText.length > 0) {
-            translate(selectLanguage, selectText)
+  useEffect(() => {
+    if (selectLanguage.length > 0 && selectText.length > 0) {
+      translate(selectLanguage, selectText);
+    }
+  }, [selectLanguage, selectText]);
+
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <div className="headingController">
+        <div className="heading">{translation.translateValue}</div>
+        <ExpandAndCrossIcon
+          handelCloseBtn={handelClose}
+          handelExpandBtn={handelExpandBtn}
+        />
+      </div>
+      <div className="selectLanguageContainer">
+        <select
+          name="languages"
+          id="languages"
+          style={{
+            width: "100%",
+            padding: "7px 10px",
+            margin: " 0% 2%",
+            borderRadius: "5px",
+            border: " 1px solid #ddd",
+          }}
+          onChange={(event) => {
+            setSelectLanguage(event.target.value);
+          }}
+        >
+          <option key={-1} value="">
+            {translation.selectLanguageValue}
+          </option>
+          {availableLanguages.map((each, i) => (
+            <option key={i} value={each.Code}>
+              {each.Display}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div
+        className={
+          isExpand ? "translateTextExpandContainer" : "translateTextContainer"
         }
-    }, [selectLanguage, selectText]);
-    
-    return(
-        <div style={{ width: '100%', height: '100%' }}>
-        <div className="headingController">
-            <div className="heading" >
-                {translation.translateValue}
-            </div>
-            <ExpandAndCrossIcon handelCloseBtn={handelClose} handelExpandBtn={handelExpandBtn} />
-        </div>
-        <div className="selectLanguageContainer">
-            <select name="languages" id="languages" style={{
-                width: '100%', padding: '7px 10px',
-                margin: ' 0% 2%',
-                borderRadius: '5px',
-                border: ' 1px solid #ddd'
-            }} onChange={(event) => {
-                setSelectLanguage(event.target.value)
-            }} >
-                <option key={-1} value="">{translation.selectLanguageValue}</option>
-                {
-                    availableLanguages.map((each, i) => (
-                        <option key={i} value={each.Code} >{each.Display}</option>
-                    ))
-                }
-            </select>
-        </div>
-        <div className={isExpand ? "translateTextExpandContainer" : "translateTextContainer"}>
-            {isLoading ? <div className="loading"><FontAwesomeIcon icon={faSpinner} className="fa-pulse" size="2xl"/></div> : translatedText}
-        </div>
+      >
+        {isLoading ? (
+          <div
+            className={isExpand ? "ExpandLoadingContainer" : "loadingContainer"}
+          >
+            <FontAwesomeIcon icon={faSpinner} className="fa-pulse" size="2xl" />
+          </div>
+        ) : (
+          <div
+            className={
+              translatedText.length > 0 ? "translateAreaAuto" : "translateArea"
+            }
+          >
+            {translatedText}
+          </div>
+        )}
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default SideSheetContent;
